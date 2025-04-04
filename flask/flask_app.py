d4991ecdc8416c4d2f482cb145a6437adcd37b1e
Achraf Hoteit <achrafhoteit8@gmail.com>
2025-04-05 00:21:01 +0300

    removed the expose port part for flask

--- flask/flask_app.py
+++ flask/flask_app.py
@@ -74,6 +74,4 @@ def summarize():
     return jsonify({"summary": summary})
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))  # default to 5000
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
