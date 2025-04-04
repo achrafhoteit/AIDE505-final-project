d009a197f7fc943675e5bf99d000a190532467dd
Achraf Hoteit <achrafhoteit2002@gmail.com>
2025-04-04 17:43:34 +0300

    added the results folder for the local ml models and configured an online database on neon

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,10 @@ const { Pool } = require('pg');
 require('dotenv').config({ path: '../.env' });
 
 const pool = new Pool({
-    connectionString: process.env.DB_URL,
-  }); // creates a new PostgreSQL connection pool that will be used to send queries to the database
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Neon
+  }
+});
 
 module.exports = pool; // export it to be used in other files
